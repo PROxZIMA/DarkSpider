4088847e6c6613f02c2d3b9b35a0388caf88eb92
PROxZIMA <pratikbpingale9075@gmail.com>
2022-09-20 23:29:14 +0530

    Update: Visualization module test cases

--- modules/tests/test_visualization.py
+++ modules/tests/test_visualization.py
@@ -2,23 +2,15 @@ import os
 import shutil
 import unittest
 
+from modules import Crawler
 from modules.checker import extract_domain, folder
-from modules.crawler import Crawler
-from modules.helpers.helper import Capturing
+from modules.helper import setup_custom_logger
 from modules.visualization import Visualization
 
 
 def test_plots(self, _func):
     """Base test function for all the plots."""
-    with Capturing() as result:
-        _func()
-
-    expected = [f"## {_func.__doc__}..."]
-    self.assertEqual(
-        expected,
-        result,
-        f"Test Fail:: {_func.__name__} returned = {result}, expected {expected}",
-    )
+    _func()
 
     output = os.path.join(self.obj.out_path, f"{_func.__name__}.png")
     self.assertTrue(
@@ -35,26 +27,32 @@ class TestVisualizationFunctions(unittest.TestCase):
         """Test Suite Setup."""
         cls._website = "http://info.cern.ch/"
         cls.out_path = out_path = folder(extract_domain(cls._website), False)
+        cls.logger = setup_custom_logger(
+            name="testlog",
+            filename=None,
+            verbose_=False,
+            filelog=False,
+            argv=None,
+        )
 
         cls.crawler = Crawler(
             website=cls._website,
-            c_depth=2,
+            proxies=None,
+            c_depth=1,
             c_pause=1,
             out_path=out_path,
             external=True,
-            thread=1,
-            logs=False,
-            verbose=False,
             exclusion=None,
+            thread=1,
+            logger=cls.logger,
         )
 
-        with Capturing() as _:
-            cls.crawler.crawl()
+        cls.crawler.crawl()
 
         cls.obj = Visualization(
             json_file=os.path.join(out_path, cls.crawler.network_file),
             out_path=out_path,
-            verbose=True,
+            logger=cls.logger,
         )
 
     @classmethod
@@ -64,43 +62,29 @@ class TestVisualizationFunctions(unittest.TestCase):
         shutil.rmtree(cls.out_path)
 
     def test_indegree_plot(self):
-        """Test visualization.indegree_plot function.
-        Scatter Plot of the indegree vs nodes of the graph.
-        """
+        """Test visualization.indegree_plot function."""
         test_plots(self, self.obj.indegree_plot)
 
     def test_indegree_bar(self):
-        """Test visualization.indegree_bar function.
-        Bar Graph of the indegree vs percentage of nodes of the graph.
-        """
+        """Test visualization.indegree_bar function."""
         test_plots(self, self.obj.indegree_bar)
 
     def test_outdegree_plot(self):
-        """Test visualization.outdegree_plot function.
-        Scatter Plot of the outdegree vs nodes of the graph.
-        """
+        """Test visualization.outdegree_plot function."""
         test_plots(self, self.obj.outdegree_plot)
 
     def test_outdegree_bar(self):
-        """Test visualization.outdegree_bar function.
-        Bar Graph of the outdegree vs percentage of nodes of the graph.
-        """
+        """Test visualization.outdegree_bar function."""
         test_plots(self, self.obj.outdegree_bar)
 
     def test_eigenvector_centrality_bar(self):
-        """Test visualization.eigenvector_centrality_bar function.
-        Bar Graph of the eigenvector centrality vs percentage of nodes of the graph.
-        """
+        """Test visualization.eigenvector_centrality_bar function."""
         test_plots(self, self.obj.eigenvector_centrality_bar)
 
     def test_pagerank_bar(self):
-        """Test visualization.pagerank_bar function.
-        Bar Graph of the pagerank vs percentage of nodes of the graph.
-        """
+        """Test visualization.pagerank_bar function."""
         test_plots(self, self.obj.pagerank_bar)
 
     def test_visualize(self):
-        """Test visualization.visualize function.
-        Visualization of the graph.
-        """
+        """Test visualization.visualize function."""
         test_plots(self, self.obj.visualize)
